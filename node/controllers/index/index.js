1894c616bc6158aa9d9b7539e94308a017378fba
Aramburu1011 <aramburu1011@hotmail.com>
2022-02-14 20:53:15 +0100

    creacion de usuarios

--- node/controllers/index/index.js
+++ node/controllers/index/index.js
@@ -4,7 +4,7 @@ const conexion = require('../../database/db');
 
 const index = async (req, res) => {
     try {
-        var sql = `SELECT * FROM templates`
+        var sql = `SELECT * FROM bets`
         conexion.query(sql, function (error, results) {
             if (error) {
                 console.log('Error con la base de datos')
@@ -20,7 +20,37 @@ const index = async (req, res) => {
 }
 
 
+const signup = async (req, res) => {
+    try {
+        req.headers['Content-Type'] = 'text/html'
+		req.headers['charset'] = 'utf-8'
+		req.charset = 'utf-8'
+        
+		let input_values = [
+            req.body['signup_name'],
+			req.body['signup_pass']
+		]
+        const sql = `INSERT INTO user (nombre, contraseña VALUES ('${input_values [0]}','${input_values [1]}');`
+        conexion.query(sql, function (error, results) {
+        if (error) {
+                console.log('Error con la base de datos')
+                console.log(input_values)
+                console.log(sql)
+                console.error(error)
+            } else {
+                console.log('Todo perfecto');
+                res.render('pages/index/index')
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
 
 module.exports = {
-    index: index
+    index: index,
+    signup : signup
 }
--- node/routes/index/index.js
+++ node/routes/index/index.js
@@ -3,6 +3,7 @@ const index = require ('../../controllers/index/index')
 
 
 router.get ('/', index.index)
+router.get ('/signup', index.signup)
 
 
 module.exports.router = router
